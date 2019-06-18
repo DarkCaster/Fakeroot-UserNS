ff280f7133b030e19fcb8d38923b8dbb22c45308
DarkCaster <dark.caster@outlook.com>
2019-06-18 04:01:43 +0300

    Revert "Try to make acl_set_file and acl_set_fd noop"
    This reverts commit 293c87f5f9100278145adc84c055aef7787f2687.

--- libfakeroot.c
+++ libfakeroot.c
@@ -1929,11 +1929,13 @@ acl_t acl_get_file(const char *path_p, acl_type_t type) {
   return (acl_t)NULL;
 }
 int acl_set_fd(int fd, acl_t acl) {
-  return 0;
+  errno = ENOTSUP;
+  return -1;
 }
 
 int acl_set_file(const char *path_p, acl_type_t type, acl_t acl) {
-  return 0;
+  errno = ENOTSUP;
+  return -1;
 }
 #endif /* HAVE_SYS_ACL_H */
 
